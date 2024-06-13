b63b1fce2c27ac989974b9fdef974005ba63cb99
Василий <Wh1lepp@gmail.com>
2024-06-13 09:18:54 +0200

    Added exaples task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array1 = {"1234", "1567", "-2", "computer science"};
+string[] array1 = {"Russia", "Denmark", "Kazan"};
+
+
+
